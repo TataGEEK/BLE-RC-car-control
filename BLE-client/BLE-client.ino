1a4d2f60bb16e4d21d67436a7c53e37275ef5a79
TataGEEK <tatageek.blog@gmail.com>
2020-05-01 22:30:28 +0200

    prvni vytvoreni repozitare

---
+++ BLE-client/BLE-client.ino
@@ -0,0 +1,361 @@
+/* instrukce
+ *  OVLADAČ
+ *  v TFT_eSPI-master/User_Setup_Select.h používám #include <User_Setups/Setup11_RPi_touch_ILI9486.h>
+ *  vývojová deska ESP32 Pico Kit
+ *  
+ */
+
+/*
+Based on:
+Neil Kolban (Bluetooth Low Energy communication)
+https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+
+Eric 0015 (Bluetooth Low Energy communication)
+https://github.com/0015/IdeasNProjects/tree/master/Esp32_BLE_to_BLE
+
+Luboš M. (Bluetooth Low Energy communication)
+https://navody.arduino-shop.cz/navody-k-produktum/esp32-a-bluetooth-low-energy-ble.html
+*/
+
+#include <SPI.h>
+
+#include <TFT_eSPI.h> // Hardware-specific library
+
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+
+#define TFT_GREY 0x5AEB // New colour
+
+
+static int offset = 30;
+static int foundDeviceCnt = 0;
+static int marginY = 6;
+
+#define nextButtonPin 12
+#define selectButtonPin 14
+
+#define right 16
+#define left 17
+#define go 26
+#define back 27
+
+int cntPos = 1;
+int prevPos = 1;
+
+#include "BLEDevice.h"
+//#include "BLEScan.h"
+
+#define ARRAYSIZE 10
+BLEAdvertisedDevice results[ARRAYSIZE];
+
+// The remote service we wish to connect to.
+static BLEUUID serviceUUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
+// The characteristic of the remote service we are interested in.
+static BLEUUID    charUUID("beb5483e-36e1-4688-b7f5-ea07361b26a8");
+
+static boolean doConnect = false;
+static boolean connected = false;
+static boolean doScan = false;
+static BLERemoteCharacteristic* pRemoteCharacteristic;
+static BLEAdvertisedDevice* myDevice;
+
+static void notifyCallback(
+  BLERemoteCharacteristic* pBLERemoteCharacteristic,
+  uint8_t* pData,
+  size_t length,
+  bool isNotify) {
+    Serial.print("Notify callback for characteristic ");
+    Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
+    Serial.print(" of data length ");
+    Serial.println(length);
+    Serial.print("data: ");
+    Serial.println((char*)pData);
+
+    String strData;
+    strData.reserve(length);
+    for(int i=0; i<length; i++){
+      strData +=(char)pData[i];
+    }
+    //printData(strData);
+    test(strData);
+}
+
+class MyClientCallback : public BLEClientCallbacks {
+  void onConnect(BLEClient* pclient) {
+  }
+
+  void onDisconnect(BLEClient* pclient) {
+    connected = false;
+    Serial.println("onDisconnect");
+  }
+};
+
+bool connectToServer() {
+    Serial.print("Forming a connection to ");
+    Serial.println(myDevice->getAddress().toString().c_str());
+    
+    BLEClient*  pClient  = BLEDevice::createClient();
+    Serial.println(" - Created client");
+
+    pClient->setClientCallbacks(new MyClientCallback());
+
+    // Connect to the remove BLE Server.
+    pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+    Serial.println(" - Connected to server");
+
+    // Obtain a reference to the service we are after in the remote BLE server.
+    BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+    if (pRemoteService == nullptr) {
+      Serial.print("Failed to find our service UUID: ");
+      Serial.println(serviceUUID.toString().c_str());
+      pClient->disconnect();
+      return false;
+    }
+    Serial.println(" - Found our service");
+
+
+    // Obtain a reference to the characteristic in the service of the remote BLE server.
+    pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+    if (pRemoteCharacteristic == nullptr) {
+      Serial.print("Failed to find our characteristic UUID: ");
+      Serial.println(charUUID.toString().c_str());
+      pClient->disconnect();
+      return false;
+    }
+    Serial.println(" - Found our characteristic");
+
+    // Read the value of the characteristic.
+    if(pRemoteCharacteristic->canRead()) {
+      std::string value = pRemoteCharacteristic->readValue();
+      Serial.print("The characteristic value was: ");
+      Serial.println(value.c_str());
+    }
+
+    if(pRemoteCharacteristic->canNotify())
+      pRemoteCharacteristic->registerForNotify(notifyCallback);
+
+    connected = true;
+}
+/**
+ * Scan for BLE servers and find the first one that advertises the service we are looking for.
+ */
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+ /**
+   * Called for each advertising BLE server.
+   */
+
+  public:
+  MyAdvertisedDeviceCallbacks(){
+    printStatus("SEARCHING...");
+  }
+   
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    printStatus("...RESULT");
+    
+    Serial.print("BLE Advertised Device found: ");
+    //Serial.println(advertisedDevice.toString().c_str());
+
+    String deviceName = advertisedDevice.getName().c_str();
+    if(deviceName.length() >1){
+      results[foundDeviceCnt] = advertisedDevice;
+      foundDeviceCnt++;
+      printContent(deviceName);
+    }
+
+    // We have found a device, let us now see if it contains the service we are looking for.
+//    if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
+//
+//      BLEDevice::getScan()->stop();
+//      myDevice = new BLEAdvertisedDevice(advertisedDevice);
+//      doConnect = true;
+//      doScan = true;
+//
+//    } // Found our server
+  } // onResult
+
+  void printContent(String deviceName){
+    int drawingOffsetY = offset * foundDeviceCnt;
+    tft.fillRect(0, drawingOffsetY, 240, 320, TFT_WHITE);
+    tft.setCursor(16, drawingOffsetY + marginY);
+    tft.setTextColor(TFT_BLACK);
+    tft.setTextSize(1);
+    tft.print(deviceName);
+  }
+
+  void printStatus(String statusText){
+    tft.fillRect(0,0,240,30, TFT_GREY);
+    tft.setCursor(6, 6);
+    tft.setTextColor(TFT_WHITE);
+    tft.setTextSize(2);
+    tft.print(statusText);
+  }
+}; // MyAdvertisedDeviceCallbacks
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting Arduino BLE Client application...");
+  
+
+  tft.begin();
+  tft.setRotation(0);
+  tft.fillScreen(TFT_WHITE);
+
+  tft.setTextColor(TFT_BLACK);
+  tft.setTextSize(2);
+  tft.print("Display test");
+
+  pinMode(nextButtonPin, INPUT);
+  pinMode(selectButtonPin, INPUT);
+
+  pinMode(left, OUTPUT);
+  pinMode(right, OUTPUT);
+  pinMode(go, OUTPUT);
+  pinMode(back, OUTPUT);
+
+  BLEDevice::init("");
+
+  // Retrieve a Scanner and set the callback we want to use to be informed when we
+  // have detected a new device.  Specify that we want active scanning and start the
+  // scan to run for 5 seconds.
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setInterval(1349);
+  pBLEScan->setWindow(449);
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(3, false);
+} // End of setup.
+
+
+// This is the Arduino main loop function.
+void loop() {
+
+
+  // If the flag "doConnect" is true then we have scanned for and found the desired
+  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are 
+  // connected we set the connected flag to be true.
+  if (doConnect == true) {
+    if (connectToServer()) {
+      printConnected(myDevice->getName().c_str());
+      Serial.println("We are now connected to the BLE Server.");
+    } else {
+      Serial.println("We have failed to connect to the server; there is nothin more we will do.");
+    }
+    doConnect = false;
+  }
+
+  // If we are connected to a peer BLE Server, update the characteristic each time we are reached
+  // with the current time since boot.
+  if (connected) {
+    String newValue = "Time since boot: " + String(millis()/1000);
+    Serial.println("Setting new characteristic value to \"" + newValue + "\"");
+    
+    // Set the characteristic's value to be the array of bytes that is actually a string.
+    pRemoteCharacteristic->writeValue(newValue.c_str(), newValue.length());
+  }else if(doScan){
+    BLEDevice::getScan()->start(0);  // this is just eample to start scan after disconnect, most likely there is better way to do it in arduino
+  }
+
+  buttonsCallBack();
+  delay(250); // Delay a second between loops.
+} // End of loop
+
+void buttonsCallBack(){
+  int nextButtonState = digitalRead(nextButtonPin);
+  int selectButtonState = digitalRead(selectButtonPin);
+
+  if(selectButtonState == HIGH){
+    myDevice = new BLEAdvertisedDevice(results[cntPos-1]);
+    doConnect = true;
+
+    int drawingOffsetY = offset * prevPos + 8;
+    tft.drawCircle(6, drawingOffsetY, 6, TFT_WHITE);
+  }
+
+  if(nextButtonState == HIGH){
+    cntPos ++;
+  }
+
+  if(cntPos > foundDeviceCnt){
+    cntPos = 1;
+  }
+
+  if(!connected){
+    if(prevPos != cntPos){
+      int drawingOffsetY = offset * prevPos + 8;
+      tft.drawCircle(6, drawingOffsetY, 6, TFT_WHITE);
+    }
+
+    if(cntPos != 0){
+      int drawingOffsetY = offset * cntPos + 8;
+      tft.drawCircle(6, drawingOffsetY, 6, TFT_RED);
+
+      prevPos = cntPos;
+    }
+  }
+}
+
+void printConnected(String deviceName){
+  tft.fillRect(0, 0, 240, 320, TFT_WHITE);
+  tft.fillRect(0, 0, 240, 30, TFT_GREY);
+  tft.setCursor(6,6);
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextSize(2);
+  tft.print("Connected");
+
+  tft.setCursor(6, 36);
+  tft.setTextColor(TFT_BLACK);
+  tft.setTextSize(2);
+  tft.print(deviceName);
+}
+
+void printData(String notiData){
+  tft.fillRect(0, 50, 240, 100, TFT_WHITE);
+  tft.setCursor(6, 70);
+  tft.setTextColor(TFT_RED);
+  tft.setTextSize(6);
+  tft.print(notiData);
+}
+
+void test(String notiData) {
+  tft.fillScreen(TFT_WHITE);
+  tft.setCursor(10, 250);
+  tft.setTextColor(TFT_RED);
+  tft.setTextSize(6);
+  tft.print(notiData);
+  
+  if (notiData == "L") {
+    tft.fillTriangle(10, 200, 40, 170, 40, 230, TFT_BLACK);
+    tft.fillTriangle(150, 200, 120, 170, 120, 230, TFT_WHITE);
+    tft.fillRect(50, 170, 60, 60, TFT_WHITE);
+    Serial.println("LEFT");
+    digitalWrite(left, HIGH);
+    digitalWrite(right, LOW);
+  }
+  if (notiData == "R") {
+    tft.fillTriangle(10, 200, 40, 170, 40, 230, TFT_WHITE);
+    tft.fillTriangle(150, 200, 120, 170, 120, 230, TFT_BLACK);
+    tft.fillRect(50, 170, 60, 60, TFT_WHITE);
+    Serial.println("RIGHT");
+    digitalWrite(left, LOW);
+    digitalWrite(right, HIGH);
+  }
+  if (notiData == "F") {
+    tft.fillTriangle(10, 200, 40, 170, 40, 230, TFT_WHITE);
+    tft.fillTriangle(150, 200, 120, 170, 120, 230, TFT_WHITE);
+    tft.fillRect(50, 170, 60, 60, TFT_BLACK);
+    Serial.println("FORWARD");
+    digitalWrite(left, LOW);
+    digitalWrite(right, LOW);
+  }
+
+  if (notiData == "G") {
+    Serial.println("GO");
+    digitalWrite(go, HIGH);
+    digitalWrite(back, LOW);
+  }
+  if (notiData == "B") {
+    Serial.println("BACK");
+    digitalWrite(go, LOW);
+    digitalWrite(back, HIGH);
+  }
+}
---
+++ BLE-server/BLE-server.ino
@@ -0,0 +1,280 @@
+/* instrukce
+ *  HODINKY
+ *  v TFT_eSPI-master/User_Setup_Select.h používám #include <User_Setups/Setup26_TTGO_T_Wristband.h>
+ *  vývojová deska ESP32 Dev Module
+ *  
+ */
+
+/*
+Based on:
+Xinyuan-LilyGO (T-Wristband example)
+https://github.com/Xinyuan-LilyGO/LilyGO-T-Wristband
+
+Neil Kolban (Bluetooth Low Energy communication)
+https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+
+Eric 0015 (Bluetooth Low Energy communication)
+https://github.com/0015/IdeasNProjects/tree/master/Esp32_BLE_to_BLE
+
+Luboš M. (Bluetooth Low Energy communication)
+https://navody.arduino-shop.cz/navody-k-produktum/esp32-a-bluetooth-low-energy-ble.html
+*/
+
+
+/****************************/
+/* T-WRISTBAND LILYGO ESP32 */
+/****************************/
+#define TP_PIN_PIN          33
+#define I2C_SDA_PIN         21
+#define I2C_SCL_PIN         22
+#define IMU_INT_PIN         38
+#define RTC_INT_PIN         34
+#define BATT_ADC_PIN        35
+#define VBUS_PIN            36
+#define TP_PWR_PIN          25
+#define LED_PIN             4
+#define CHARGE_PIN          32
+
+/***********/
+/* BATTERY */
+/***********/
+int vref = 1100;
+
+/*****************************/
+/* GYROSCOPE & ACCELEROMETER */
+/*****************************/
+#include <SPI.h>
+#include <Wire.h>
+#include "sensor.h"
+char buff[256];
+String instrukce;
+String last_instruction;
+
+/****************/
+/* TOUCH BUTTON */
+/****************/
+bool pressed = false;
+uint32_t pressedTime = 0;
+bool charge_indication = false;
+uint8_t func_select = 0;
+
+uint32_t targetTime = 0;       // for next 1 second timeout
+bool initial = 1;
+
+
+/***********/
+/* DISPLAY */
+/***********/
+#include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
+TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
+
+/*************/
+/* BLUETOOTH */
+/*************/
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      BLEDevice::startAdvertising();
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
+  
+  tft.init();
+  tft.setRotation(1);
+
+  targetTime = millis() + 1000;
+
+  pinMode(TP_PIN_PIN, INPUT);
+  //! Must be set to pull-up output mode in order to wake up in deep sleep mode
+  pinMode(TP_PWR_PIN, PULLUP);
+  digitalWrite(TP_PWR_PIN, HIGH);
+  pinMode(LED_PIN, OUTPUT);
+
+
+  // Create the BLE Device
+  BLEDevice::init("ESP32 RC-CAR");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+String getVoltage()
+{
+    uint16_t v = analogRead(BATT_ADC_PIN);
+    float battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
+    return String(battery_voltage) + " V";
+}
+
+String ovladani()
+{
+    readMPU9250();
+
+    if (((int)1000 * IMU.ax > -300) & ((int)1000 * IMU.ax < 300)) {
+      if (last_instruction != instrukce) {
+        tft.fillScreen(TFT_BLACK);
+        last_instruction = instrukce;
+      }
+      tft.fillRect(60, 20, 40, 40, TFT_BLUE);
+      instrukce = "G";
+      return String(instrukce);
+    }
+    if ((int)1000 * IMU.ay < -200) {
+      if (last_instruction != instrukce) {
+        tft.fillScreen(TFT_BLACK);
+        last_instruction = instrukce;
+      }
+      tft.fillTriangle(10, 40, 30, 20, 30, 60, TFT_WHITE);
+      instrukce = "L";
+      return String(instrukce);
+    } else if ((int)1000 * IMU.ay > 200) {
+      if (last_instruction != instrukce) {
+        tft.fillScreen(TFT_BLACK);
+        last_instruction = instrukce;
+      }
+      tft.fillTriangle(150, 40, 120, 20, 120, 60, TFT_WHITE);
+      instrukce = "R";
+      return String(instrukce);
+    } else {
+      if (last_instruction != instrukce) {
+        tft.fillScreen(TFT_BLACK);
+        last_instruction = instrukce;
+      }
+      tft.fillRect(60, 20, 40, 40, TFT_WHITE);
+      instrukce = "F";
+      return String(instrukce);
+    }
+}
+
+
+void loop() {
+//pouze pro ladění, pak přesunout na psávné místo
+instrukce = ovladani();
+tft.drawCentreString(getVoltage(), 130, 70, 1); // Next size up font 2
+
+snprintf(buff, sizeof(buff), "x %.2f  %.2f  %.2f", (int)1000 * IMU.ax, IMU.gx, IMU.mx);
+    tft.drawString(buff, 0, 70);
+
+    // klepnutí na hodinky blikne diodou
+    if (IMU.gx > 200) {
+      digitalWrite(LED_PIN, HIGH);
+      delay(100);
+      digitalWrite(LED_PIN, LOW);
+    }
+
+    if (digitalRead(TP_PIN_PIN) == HIGH) {
+      if (!pressed) {
+        initial = 1;
+        targetTime = millis() + 1000;
+        func_select = func_select + 1 > 3 ? 0 : func_select + 1;
+        digitalWrite(LED_PIN, HIGH);
+        delay(100);
+        digitalWrite(LED_PIN, LOW);
+        pressed = true;
+        pressedTime = millis();
+      } else {
+        if (millis() - pressedTime > 3000) {
+          tft.fillScreen(TFT_BLACK);
+          tft.fillScreen(TFT_BLACK);
+          tft.drawString("Dlouhý stisk",  50, tft.height() / 2 - 10);
+          tft.drawString("Go to Sleep",  53, tft.height() / 2 + 10);
+          Serial.println("Go to Sleep");
+          delay(3000);
+          tft.writecommand(ST7735_SLPIN);
+          tft.writecommand(ST7735_DISPOFF);
+          esp_sleep_enable_ext1_wakeup(GPIO_SEL_33, ESP_EXT1_WAKEUP_ANY_HIGH);
+          esp_deep_sleep_start();
+        }
+      }
+    } else {
+      pressed = false;
+    }
+  
+  // notify changed value
+  if (deviceConnected) {
+
+    tft.drawString("Connected",  tft.width() / 2 - 30, 0);
+
+    // vytvoření dočasné proměnné, do které je převedna zpráva na znaky
+    char instrukceChar[instrukce.length() + 1];
+    instrukce.toCharArray(instrukceChar, instrukce.length() + 1);
+    // přepsání zprávy do BLE služby
+    pCharacteristic->setValue(instrukceChar);
+    pCharacteristic->notify();
+    delay(500); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+  } else {
+    tft.drawString("Cekam na pripojeni bluetooth",  0, 0);
+  }
+
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected) {
+    delay(500); // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+
+    // přidáno
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_WHITE);
+    tft.drawString("start advertising",  0, tft.height() / 2 - 20);
+
+    Serial.println("start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected) {
+    // do stuff here on connecting
+    oldDeviceConnected = deviceConnected;
+  }
+}
